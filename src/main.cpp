eae795e803f8edadf25ed359ce428b6b296fbb92
Kartikey Subramanium <kartikey@tutanota.com>
2022-05-09 23:52:01 +0530

    switch to cpp

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QtQml>
+#include <QUrl>
+#include <KLocalizedContext>
+#include <KLocalizedString>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
+
+    return app.exec();
+}
